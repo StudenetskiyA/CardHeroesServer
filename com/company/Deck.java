9054343be66bb3c4bb0ab6b80ce33c206a6ccaca
StudenetskiyA <wow.dayre@gmail.com>
2017-02-02 22:53:07 +0500

    and it

---
+++ com/company/Deck.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+/**
+ * Created by StudenetskiyA on 30.12.2016.
+ */
+public class Deck {
+    public ArrayList<Card> cards = new ArrayList<>();
+
+    public String name;
+
+    public Deck(String _name){
+        name=_name;
+    }
+
+    public int getCardExpiried(){
+        return cards.size();
+    }
+
+    public Card searchCard(String name){
+        for (int i=0;i<=cards.size();i++){
+            if (cards.get(i).name.equals(name)) return cards.get(i);
+        }
+        return null;
+    }
+
+    public boolean haveTopDeck(){
+        if (cards.size()==0) return false;
+        else return true;
+    }
+
+    public Card topDeck(int depth){
+        if (cards.size()>=depth)
+            return cards.get(cards.size()-depth);
+        else {
+           // Main.printToView(0,"Закончилась колода");
+        }
+        return null;
+    }
+
+    public Card topDeck(){
+        return topDeck(1);
+    }
+
+    public Card drawTopDeck(){
+        Card tmp=cards.get(cards.size()-1);
+        cards.remove(cards.size()-1);
+        return tmp;
+    }
+
+    public void removeTopDeck(){
+        if (haveTopDeck())
+            cards.remove(cards.size()-1);
+    }
+
+    public void putOnBottomDeck(Card _card){
+        cards.add(0,_card);
+    }
+
+    public void suffleDeck(int n){
+        //Until server know nothing
+        for (int i=0;i<cards.size();i++){
+            byte[] b = (cards.get(i).name+i+n).getBytes();
+            try {
+                byte[] hash = MessageDigest.getInstance("MD5").digest(b);
+                String a=DatatypeConverter.printHexBinary(hash);
+                cards.get(i).hash=a;
+                // System.out.println(cards.get(i).name+"/"+a);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        Collections.sort(cards, Comparator.comparing(o -> o.hash));
+    }
+
+    public String deckToString(){
+        String tmp="";
+        for (Card card: cards
+                ) {
+            tmp+=card.name;
+            tmp+=",";
+        }
+        return tmp;
+    }
+
+
+}
